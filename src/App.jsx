c9b46bc8ee07e6d346b79c706e266a6093c1f126
aysh-mzmdr <ayush.mazumdar111@gmail.com>
2026-02-28 13:07:08 +0530

    Added Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react'
 import Footer from './components/Footer/Footer'
 import Discord from './components/Discord/Discord'
 import Schedule from './components/Schedule/Schedule'
@@ -15,13 +16,17 @@ import Venue from './components/Venue/Venue'
 import CustomScrollbar from './components/CustomScrollbar/CustomScrollbar';
 // import CursorFollower from './components/CursorFollower';
 
+import Preloader from './components/Preloader/Preloader'
+
 import LandingPage from './pages/LandingPage'
 import Hackatron2 from './components/Hackatron2/Hackatron2'
 import Prizes from './components/Prizes/Prizes'
 function App() {
-
+  const [loading, setLoading] = useState(true);
   return (
     <>
+     {loading && <Preloader onComplete={() => setLoading(false)} />}
+      <div className={`transition-opacity duration-700 ${loading ? 'opacity-0 h-0 overflow-hidden' : 'opacity-100'}`}></div>
       <CustomScrollbar />
       {/* <CursorFollower /> */}
       <Navbar />
